6b15eba89e9a6651b98cb4d07498c60cbe7d9954
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2017-08-10 16:33:20 +0900

    Blogにshowを追加
    ・BlogsControllerにshowアクションを追加
    ・showアクションのviewを追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,17 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show]
+
   def index
     @blogs = Blog.all
+  end
+
+  def show
+    @entries = @blog.entries
+  end
+
+  private
+
+  def set_blog
+    @blog = Blog.find(params[:id])
   end
 end
