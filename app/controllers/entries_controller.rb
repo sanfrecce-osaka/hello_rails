a59c83b9d2d8e482c182d037e5a4918b8bebb6cf
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2017-08-11 10:48:00 +0900

    Entryのnewを修正
    ・EntriesControllerのnewでblogを取得するように修正
    ・リンク先のパスを修正
    ・フォームの文言を修正

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,5 +1,6 @@
 class EntriesController < ApplicationController
   before_action :set_entry, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:new]
 
   # GET /entries/1
   def show
@@ -44,6 +45,10 @@ class EntriesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_entry
       @entry = Entry.find(params[:id])
+    end
+
+    def set_blog
+      @blog = Blog.find(params[:blog_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
